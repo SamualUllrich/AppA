41556a41ea724d84d77a85c98d8772c22abafd9f
Sam <ullrichsam@gmail.com>
2023-08-06 19:59:32 -0700

    Add project files.

---
+++ AppA/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppA
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppA/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppA
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
