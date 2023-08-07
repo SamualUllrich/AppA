570c7c8c00ac49d81c5749d7400fff5c41f981ab
Sam <ullrichsam@gmail.com>
2023-08-07 00:55:52 -0700

    Added EmbedIOServer class for server services

---
+++ AppA/Server/EmbedIOServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppA.Server
+{
+    internal class Class1
+    {
+    }
+}
