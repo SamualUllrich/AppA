76b3fa0684c15a6d744ab280bce20c4c73d21502
Sam <ullrichsam@gmail.com>
2023-08-09 22:59:49 -0700

    Added socket module and server logic

--- AppA/App.xaml.cs
+++ AppA/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace AppA
+﻿using EmbedIO;
+using EmbedIO.Actions;
+
+namespace AppA
 {
     public partial class App : Application
     {
@@ -6,7 +9,23 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            // Start the EmbedIO web server asynchronously
+            _ = StartWebServerAsync();
+
+            MainPage = new MainPage();
+        }
+
+        private async Task StartWebServerAsync()
+        {
+            var webServer = new WebServer(o => o
+                .WithUrlPrefix("http://localhost:8080/")
+                .WithMode(HttpListenerMode.EmbedIO))
+                //.WithModule(new ActionModule("/", HttpVerbs.Any, ctx => ctx.SendStringAsync("Welcome to Tic Tac Toe API")))
+                .WithWebApi("/api", m => m.RegisterController<GameController>())
+                .WithModule(new TicTacToeWebSocketModule("/ws")); 
+
+            // Start the EmbedIO server
+            await webServer.RunAsync();
         }
     }
 }
---
+++ AppA/TicTacToeWebSocketModule.cs
@@ -0,0 +1,44 @@
+﻿using EmbedIO.WebSockets;
+using System.Text;
+
+namespace AppA
+{
+    public class TicTacToeWebSocketModule : WebSocketModule
+    {
+        public TicTacToeWebSocketModule(string urlPath)
+            : base(urlPath, true)
+        {
+        }
+
+        protected override Task OnMessageReceivedAsync(
+            IWebSocketContext context,
+            byte[] rxBuffer,
+            IWebSocketReceiveResult rxResult)
+        {
+            // Handle the received message
+            string message = Encoding.UTF8.GetString(rxBuffer);
+            // Implement your logic to handle the received message
+            return Task.CompletedTask;
+        }
+
+        protected override Task OnClientConnectedAsync(IWebSocketContext context)
+        {
+            // Handle client connection
+            // For example, send a welcome message to the client
+            return SendAsync(context, "Welcome to the Tic Tac Toe game!");
+        }
+
+        protected override Task OnClientDisconnectedAsync(IWebSocketContext context)
+        {
+            // Handle client disconnection
+            // For example, notify other players that someone left
+            return Task.CompletedTask;
+        }
+
+        private Task SendToOthersAsync(IWebSocketContext context, string payload)
+        {
+            // Broadcast the payload to all connected clients except the given context
+            return BroadcastAsync(payload, c => c != context);
+        }
+    }
+}
